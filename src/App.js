740a78af014838dcbf50e98ca624c7768cb4315f
Laia Lloret <laialloret95@gmail.com>
2021-06-03 09:49:04 +0200

    remove space

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import Declarative from './components/Cards/Declarative/Declarative';
-import Components from './components/Cards/Components/Components';
-import JSX from './components/Cards/JSX/JSX';
-import SingleWay from './components/Cards/SingleWay/SingleWay';
 import Header from './components/Header/Header';
 import CardContainer from './components/CardContainer/CardContainer';
 
 function App() {
   return (
     <main>
-    <Header />
-    <CardContainer>
-        <Declarative />
-        <Components />
-        <SingleWay />
-        <JSX />
-    </CardContainer>
-  </main>
+        <Header />
+        <CardContainer />
+    </main>
   );
 }
 
