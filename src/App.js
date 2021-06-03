d0dbf28d616fc14d21906f277a1dfd411cfb18f1
Laia Lloret <laialloret95@gmail.com>
2021-06-03 09:43:06 +0200

    build components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import './App.css';
+import Declarative from './components/Cards/Declarative/Declarative';
+import Components from './components/Cards/Components/Components';
+import JSX from './components/Cards/JSX/JSX';
+import SingleWay from './components/Cards/SingleWay/SingleWay';
+import Header from './components/Header/Header';
+import CardContainer from './components/CardContainer/CardContainer';
 
 function App() {
   return (
     <main>
-    <section className="head-container">
-        <nav className="navbar">
-            <img src="/images/ironhack-logo.svg" alt="ironhack-logo"/>
-            <img src="/images/menu-top.svg" alt="ironhack-menu"/>
-        </nav>
-        <div className="hello-container">
-            <h1>Say hello to ReactJS</h1>
-            <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
-            <button>Awesome!</button>
-        </div>             
-    </section>
-    <section className="body-container">
-        <div>
-            <img src="/images/icon1.png" alt="icon1"/>
-            <h3>Declarative</h3>
-            <p>React makes it painless to create interactive UIs.</p>
-        </div>
-        <div>
-            <img src="/images/icon2.png" alt="icon2"/>
-            <h3>Components</h3>
-            <p>Build encapsuated components that manage their state.</p>
-        </div>
-        <div>
-            <img src="/images/icon3.png" alt="icon3"/>
-            <h3>Single-Way</h3>
-            <p>A set of immutable values are passed to the component's.</p>
-        </div>
-        <div>
-            <img src="/images/icon4.png" alt="icon4"/>
-            <h3>JSX</h3>
-            <p>Statically-typed, designed to run on modern browsers.</p>
-        </div>
-    </section>
+    <Header />
+    <CardContainer>
+        <Declarative />
+        <Components />
+        <SingleWay />
+        <JSX />
+    </CardContainer>
   </main>
   );
 }
